04773b47c3b5f45e689407ea9a326a3f9c5e2cdf
Juha Laakko <entropai@gmail.com>
2013-01-26 21:50:17 +0200

    Forgotten file

--- AudioSystem.py
+++ AudioSystem.py
@@ -1,9 +1,12 @@
 import pygame
 
+SONG_END = pygame.USEREVENT+1
+
 class MusicPlayer:
 
     def __init__(self):
         self.tracks = ["audio/26.ogg"]
+        pygame.mixer.music.set_endevent(SONG_END)
 
     def PlayTrack(self):
         pygame.mixer.music.load(self.tracks[0])
